8048a04b699d0b8f167472b765c4b62b5b659c2c
Sahiljain24 <24sahilj@gmail.com>
2025-03-03 21:56:41 +0530

    [Sahil] Add. added all the code for uc_03 day 3

---
+++ payroll/src/main/java/com/spring/payroll/DTO/EmployeePayrollDTO.java
@@ -0,0 +1,14 @@
+package com.spring.payroll.DTO;
+
+
+
+
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class EmployeePayrollDTO {
+    private String name;
+    private double salary;
+}
--- payroll/src/main/java/com/spring/payroll/service/EmployeePayrollService.java
+++ payroll/src/main/java/com/spring/payroll/service/EmployeePayrollService.java
@@ -1,12 +1,11 @@
 package com.spring.payroll.service;
 
-
-
+import com.spring.payroll.DTO.EmployeePayrollDTO;
 import com.spring.payroll.Model.EmployeePayrollData;
-import com.spring.payroll.repository.UserRepository;
-
+import com.spring.payroll.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import java.util.List;
 import java.util.Optional;
 
@@ -14,11 +13,11 @@ import java.util.Optional;
 public class EmployeePayrollService {
 
     @Autowired
-    private EmployeeRepository employeeRepository;
+    private EmployeeRepository employeeRepository; // Inject repository
 
     // Get all employees
     public List<EmployeePayrollData> getAllEmployees() {
-        return employeeRepository.findAll();
+        return employeeRepository.findAll(); // Fetch from database
     }
 
     // Get employee by ID
@@ -26,17 +25,18 @@ public class EmployeePayrollService {
         return employeeRepository.findById(id);
     }
 
-    // Create new employee
-    public EmployeePayrollData createEmployee(EmployeePayrollData employee) {
-        return employeeRepository.save(employee);
+    // Create new employee (Convert DTO to Model)
+    public EmployeePayrollData createEmployee(EmployeePayrollDTO employeeDTO) {
+        EmployeePayrollData newEmployee = new EmployeePayrollData(null, employeeDTO.getName(), employeeDTO.getSalary());
+        return employeeRepository.save(newEmployee);
     }
 
     // Update employee
-    public EmployeePayrollData updateEmployee(Long id, EmployeePayrollData updatedEmployee) {
+    public EmployeePayrollData updateEmployee(Long id, EmployeePayrollDTO updatedDTO) {
         return employeeRepository.findById(id).map(employee -> {
-            employee.setName(updatedEmployee.getName());
-            employee.setSalary(updatedEmployee.getSalary());
-            return employeeRepository.save(employee);
+            employee.setName(updatedDTO.getName());
+            employee.setSalary(updatedDTO.getSalary());
+            return employeeRepository.save(employee); // Save updated employee
         }).orElseThrow(() -> new RuntimeException("Employee not found"));
     }
 
